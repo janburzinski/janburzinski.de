d7b0fa454663f008768eee165cb181951056909d
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2026-04-22 22:15:26 +0200

    feat: add Docker deployment support

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isNodeBuild = process.env.BUILD_TARGET === 'node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,16 +11,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// See https://kit.svelte.dev/docs/adapter-cloudflare for options.
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: isNodeBuild
+			? nodeAdapter()
+			: adapter({
+				// Keep Cloudflare deploys working unless we explicitly build for Node/Docker.
+				routes: {
+					include: ['/*'],
+					exclude: ['<all>']
+				}
+			})
 	}
 };
 
