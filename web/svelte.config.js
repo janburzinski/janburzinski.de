8a804c1532ddeb28d4ec4d4893d17fc9853be0ea
Jan Burzinski <156842394+janburzinski@users.noreply.github.com>
2026-05-14 12:30:23 +0200

    Simplify to Vercel-only deployment

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,25 +1,12 @@
-import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
-import nodeAdapter from '@sveltejs/adapter-node';
-import vercelAdapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const buildTarget = process.env.BUILD_TARGET ?? 'cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: buildTarget === 'node'
-			? nodeAdapter()
-			: buildTarget === 'vercel'
-				? vercelAdapter()
-				: cloudflareAdapter({
-					routes: {
-						include: ['/*'],
-						exclude: ['<all>']
-					}
-				})
+		adapter: adapter()
 	}
 };
 
